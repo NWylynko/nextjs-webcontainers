84c6da3b61ddd80da4aed88d97b586ec025b910d
Nick Wylynko <nickwylynko@clerk.dev>
2025-08-22 03:19:23 +0800

    Add link to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Webcontainers</h1>
+      <Link href="https://github.com/NWylynko/nextjs-webcontainers">Repo</Link>
     </main>
   );
 }
